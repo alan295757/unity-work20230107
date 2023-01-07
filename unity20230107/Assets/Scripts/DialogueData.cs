86af4f4b1dacd78ba175b61e43b19967541513f9
alan295757 <113963477+alan295757@users.noreply.github.com>
2023-01-07 17:06:51 +0800

    0107完成協同

--- unity20230107/Assets/Scripts/DialogueData.cs
+++ unity20230107/Assets/Scripts/DialogueData.cs
@@ -1,16 +1,16 @@
-using UnityEngine;
+ï»¿using UnityEngine;
 
 namespace JAY
 {
     /// <summary>
-    /// ¹ï¸Ü¸ê®Æ
+    /// å°è©±è³‡æ–™
     /// </summary>
     [CreateAssetMenu(menuName = "JAY/Dialogue Data", fileName = "New Dialogue Data")]
     public class DialogueData : ScriptableObject
     {
-        [Header("¹ï¸ÜªÌ¦WºÙ")]
+        [Header("å°è©±è€…åç¨±")]
         public string dialogueName;
-        [Header("¹ï¸ÜªÌ¤º®e"), TextArea(2, 10)]
+        [Header("å°è©±è€…å…§å®¹"), TextArea(2, 10)]
         public string[] dialogueContents;
     }
 }
---
+++ unity20230107/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+namespace JAY
+{
+    public class DialogueSystem : MonoBehaviour
+    {
+        [SerializeField, Header("對話間隔"), Range(0, 0.5f)]
+        private float dialogueIntervalTime = 0.1f;
+        [SerializeField, Header("開頭對話")]
+        private DialogueData dialogueOpening;
+        [SerializeField, Header("對話按鍵")]
+        private KeyCode dialogueKey = KeyCode.Space;
+
+        private WaitForSeconds dialogueInterval => new WaitForSeconds(dialogueIntervalTime);
+
+        private CanvasGroup groupDialogue;
+        private TextMeshProUGUI textName;
+        private TextMeshProUGUI textContent;
+        private GameObject goTriangle;
+
+        private PlayerInput playerInput;
+
+        private UnityEvent onDialogueFinish;
+
+        private void Awake()
+        {
+            groupDialogue = GameObject.Find("畫布對話系統").GetComponent<CanvasGroup>();
+            textName = GameObject.Find("對話者名稱").GetComponent<TextMeshProUGUI>();
+            textContent = GameObject.Find("對話內容").GetComponent<TextMeshProUGUI>();
+            goTriangle = GameObject.Find("對話完成圖示");
+            goTriangle.SetActive(false);
+
+            playerInput = GameObject.Find("PlayerCapsule").GetComponent<PlayerInput>();
+
+            StartDialogue(dialogueOpening);
+        }
+        /// <summary>
+        /// 開始對話
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="_onDialogueFinish"></param>
+        public void StartDialogue(DialogueData data, UnityEvent _onDialogueFinish = null)
+        {
+            playerInput.enabled = false;
+            StartCoroutine(FadeGroup());
+            StartCoroutine(TypeEffect(data));
+            onDialogueFinish = _onDialogueFinish;
+        }
+        private IEnumerator FadeGroup(bool fadeIn = true)
+        {
+            float increase = fadeIn ? +0.1f : -0.1f;
+
+            for (int i = 0; i < 10; i++)
+            {
+                groupDialogue.alpha += increase;
+                yield return new WaitForSeconds(0.04f);
+            }
+        }
+
+        private IEnumerator TypeEffect(DialogueData data)
+        {
+            textName.text = data.dialogueName;
+
+            for (int j = 0; j < data.dialogueContents.Length; j++)
+            {
+                textContent.text = "";
+                goTriangle.SetActive(false);
+
+                string dialogue = data.dialogueContents[j];
+
+                for (int i = 0; i < dialogue.Length; i++)
+                {
+                    textContent.text += dialogue[i];
+                    yield return dialogueInterval;
+                }
+
+                goTriangle.SetActive(true);
+
+                while (!Input.GetKeyDown(dialogueKey))
+                {
+                    yield return null;
+                }
+
+                print("<color=#993300>玩家按下按鍵！</color");
+            }
+
+            StartCoroutine(FadeGroup(false));
+
+            playerInput.enabled = true;
+
+            onDialogueFinish.Invoke();
+        }
+    }
+}
